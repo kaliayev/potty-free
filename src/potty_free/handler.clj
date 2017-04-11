9e72a9d51a282ec9abf85cdc72e55d15885ddec0
kaliayev <calinfraser@gmail.com>
2017-04-11 18:48:27 -0300

    update to use async

--- src/potty_free/handler.clj
+++ src/potty_free/handler.clj
@@ -2,43 +2,43 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [gpio.core :as gpio]
+            [clojure.core.async :as a]
             [cheshire.core :as json]
             [clj-http.client :as http]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:gen-class))
 
-(def default-tid :first)
+(def this-tid "third")
 
 (def port-num 17)
 
 (def server
-  "http://dcb0e416.ngrok.io/potty")
+  (str "http://fb71d3f7.ngrok.io/toilet/" this-tid))
 
 (def timeout
   5000)
 
-(defn parse-gpio
-  [port]
-  (let [value (gpio/read-value port)]
-    (cond (= value :high) :occupied
-          (= value :low) :free)))
+(defn parse-value
+  [value]
+  (cond (= value :high) false
+        (= value :low) true))
 
 (defn -main
   [& args]
-  (let [tid (or (first args) default-tid)]
-    (try
-      (loop []
-        (let [gpio-port (gpio/open-channel-port port-num)]
-          (do
-            (println "port " port-num " is open")
-            (http/post server {:body (json/generate-string
-                                      {:state (parse-gpio gpio-port)
-                                       :toilet tid})
-                               :throw-exceptions false
-                               :content-type :json
-                               :as :json})
-            (gpio/close! gpio-port)
-            (println "port " port-num " is closed")
-            (Thread/sleep timeout)
-            (recur))))
-      (catch Exception e (println e)))))
+  (let [tid (or (first args) this-tid)]
+    (a/go (loop []
+            (let [gpio-port (gpio/open-channel-port port-num)
+                  _ (gpio/set-direction! gpio-port :in)
+                  _ (gpio/set-edge! gpio-port :both)
+                  ch (gpio/create-edge-channel gpio-port)]
+              (when-let [value (a/<! ch)]
+                (http/post server {:body (json/generate-string
+                                          {:state (parse-value value)})
+                                   :throw-exceptions false
+                                   :content-type :json
+                                   :as :json}))
+              (a/close! ch)
+              (gpio/close! (gpio/open-channel-port port-num))
+              (println "port " port-num " is closed")
+              (Thread/sleep timeout))
+            (recur)))))
