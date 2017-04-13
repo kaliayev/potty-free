4430cc40cf40b3da79fa513b30a1907b6b96c5f3
kaliayev <calinfraser@gmail.com>
2017-04-13 00:18:07 -0300

    catch throwables, add command line args

--- src/potty_free/handler.clj
+++ src/potty_free/handler.clj
@@ -8,12 +8,15 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:gen-class))
 
-(def this-tid "third")
+(def this-tid "1")
 
 (def port-num 17)
 
+(def host
+  "https://us-east-1-dev-pnq.cxengagelabs.net/toilet/")
+
 (def server
-  (str "http://fb71d3f7.ngrok.io/toilet/" this-tid))
+  (str "https://us-east-1-dev-pnq.cxengagelabs.net/toilet/" this-tid))
 
 (def timeout
   5000)
@@ -30,30 +33,36 @@
         (= value :low) true))
 
 (defn process-gpio-chan
-  [ch gpio-port]
+  [ch gpio-port url]
   (loop []
     (when-let [value (a/<!! ch)]
-      (let [body (json/generate-string
-                  {:status (-> gpio-port
-                               gpio/read-value
-                               parse-value)})
-            _ (println (str "\n\n" body "\n"))
-            result (http/post server {:body body
-                                      :throw-exceptions false
-                                      :content-type :json
-                                      :as :json})]
-        (println (str "\n\n" result "\n"))))
+      (try (let [body (json/generate-string
+                       {:status (-> gpio-port
+                                    gpio/read-value
+                                    parse-value)})
+                 _ (println (str "\n\n" body "\n"))
+                 result (http/post url {:body body
+                                        :throw-exceptions false
+                                        :content-type :json
+                                        :as :json})]
+             (println (str "\n\n" result "\n")))
+           (catch Throwable t
+             (println (pr-str t) "\n\nPost Request Failed."))))
     (recur)))
 
 (defn -main [& args]
   (let [tid (or (first args) this-tid)
+        host (or (second args) host)
+        url (str host tid)
+        _ (println "Host: " host "\nUrl: " url)
         gpio-port (-> (gpio/open-channel-port port-num)
                       (gpio/set-direction! :in)
                       (gpio/set-edge! :both))
         gpio< (gpio/create-edge-channel gpio-port)]
     (add-shutdown-hook (do (a/close! gpio<)
-                           (gpio/close! gpio-port)))
-    (try (process-gpio-chan gpio< gpio-port)
+                           (gpio/close! gpio-port)
+                           (println "\n\nUser Terminated!!!")))
+    (try (process-gpio-chan gpio< gpio-port url)
          (catch Throwable e
            (println (pr-str e) "\n\nUser Terminated!!!"))
          (finally (do (a/close! gpio<)
